5fc8aa85703dd1e7b792a036c331c807213e1da6
Collins Kibet <kollcibe05@gmail.com>
2025-09-30 14:31:48 +0300

    Solved commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Accept': "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+       document.body.innerHTML = `<p>${data.id}</p>`
+    
+    })
+    .catch(error => {
+        document.body.innerHTML = `<p>${error.message}</p>`
+    }
+    )
+}
+let name = 'collins'
+let email = 'collinsKibet@gmail.com'
+console.log(submitData(name,email))
